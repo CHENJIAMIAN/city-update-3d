ef032bb7953b2f27eda74cb2ca55ba0010bc076e
CHENJIAMIAN <571574085@qq.com>
2022-08-25 08:57:05 +0000

    Deploying to gh-pages from @ CHENJIAMIAN/city-update-3d@45af0382e06f44de873180d9c3585c2cf622ab63 🚀

--- cesium/Workers/createFrustumGeometry.js
+++ cesium/Workers/createFrustumGeometry.js
@@ -22,4 +22,4 @@
  * Portions licensed separately.
  * See https://github.com/CesiumGS/cesium/blob/main/LICENSE.md for full licensing details.
  */
-define(["./defaultValue-4607806f","./FrustumGeometry-d0e37f0a","./Transforms-c450597e","./Matrix2-21f90abf","./RuntimeError-cef79f54","./ComponentDatatype-4028c72d","./WebGLConstants-f100e3dd","./_commonjsHelpers-a32ac251","./combine-fc59ba59","./GeometryAttribute-3c090c07","./GeometryAttributes-acac33d2","./Plane-1c5eb32d","./VertexFormat-75e8069c"],(function(e,t,r,a,c,n,o,u,m,f,s,d,i){"use strict";return function(r,a){return e.defined(a)&&(r=t.FrustumGeometry.unpack(r,a)),t.FrustumGeometry.createGeometry(r)}}));
+define(["./defaultValue-4607806f","./FrustumGeometry-d0e37f0a","./Transforms-c450597e","./Matrix2-21f90abf","./RuntimeError-cef79f54","./ComponentDatatype-4028c72d","./WebGLConstants-f100e3dd","./commonjsHelpers-a32ac251","./combine-fc59ba59","./GeometryAttribute-3c090c07","./GeometryAttributes-acac33d2","./Plane-1c5eb32d","./VertexFormat-75e8069c"],(function(e,t,r,a,c,n,o,u,m,f,s,d,i){"use strict";return function(r,a){return e.defined(a)&&(r=t.FrustumGeometry.unpack(r,a)),t.FrustumGeometry.createGeometry(r)}}));
